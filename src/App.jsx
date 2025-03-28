676f6d392801df0538518edfc6145777fd46851f
Abhishek <abhishekkrishna0001@gmail.com>
2025-03-28 11:26:59 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Namaste everyone, let us build netflix gpt.
+    </div>
+  );
+}
+
+export default App;
